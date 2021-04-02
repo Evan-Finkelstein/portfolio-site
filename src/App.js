74d49644bf4b76816fdf8ccfc72d7f32bee8ace6
Evan Finkelstein <evanfinkelstein91@gmail.com>
2021-04-01 18:03:06 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ export default class App extends Component {
 
         <Router>
           <Header />
-
           <DashMenu />
           <div className='display'>
             <Switch>
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
-
 import './App.css';
+import { Link } from 'react-router-dom'
+
+
+
 
 export default class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+
+        this.togglePanel = this.togglePanel.bind(this);
+    }
+
+    togglePanel(e) {
+        this.setState({ open: !this.state.open })
+    }
     render() {
         return (
             <div >
 
                 <div className='header'>
-                    <span>Westbrook Community Garden</span>
+                    <div>Westbrook Community Garden</div>
+                    {/* <div>
+                        {!this.state.open ? <div onClick={(e) => this.togglePanel(e)} className='menu'><span className='menu-icon'>Menu</span></div>
+                            : <div onClick={(e) => this.togglePanel(e)} className='menu'><span className='menu-icon'>Close</span></div>}
+                        {this.state.open ? (
+                            <div className='dash'>
+                                <Link to={'/'}>Home</Link>
+                                <Link to={'/tips'}> Tips </Link>
+                                <Link to={'/about'}> About</Link>
+                                <Link to={'/links'}>Links</Link>
+                                <Link to={'/gallery'}>Gallery</Link>
+
+                            </div>
+                        ) : null}
+                    </div> */}
                 </div>
+
 
             </div>
         )
